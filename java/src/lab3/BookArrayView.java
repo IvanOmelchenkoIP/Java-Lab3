50d9fcb9140b0950d994cd5989f5a8ee523a58c5
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-09 21:41:05 +0300

    Add BookArrayView class methods
    Add BookArrayView class methods for further implementation

--- java/src/lab3/BookArrayView.java
+++ java/src/lab3/BookArrayView.java
@@ -1,5 +1,36 @@
 package lab3;
 
 public class BookArrayView {
+	
+	private final String ARRAY_FORMED = "Array of books has been filled successfully. Amout of books in the array:";
+	
+	private final String SUCCESS_SELECT_BY_AUTHOR = "Books written by author";
+	private final String SUCCESS_SELECT_BY_PUBLISHER = "Books published by publisher";
+	private final String SUCCESS_SELECT_AFTER_YEAR = "Books published after year";
+	private final String SUCCESS_SORT_BY_PUBLISHER = "Sorted array of books by the publisher:";
+	
+	private final String ERR_SELECT_BY_AUTHOR = "There is no data on books written by author";
+	private final String ERR_SELECT_BY_PUBLISHER = "There is no data on books published by publisher";
+	private final String ERR_SELECT_AFTER_YEAR = "There is no data on books published after year";
+	private final String ERR_SORT_BY_PUBLISHER = "There is not a single book in the array to sort...";
+	
+	public void printInitMessage() {
+		
+	}
+	
+	public void printSelectByAuthorMessage() {
+		
+	}
 
+	public void printSelectByPublisherMessage() {
+	
+	}
+
+	public void printSelectAfterYearMessage() {
+	
+	}
+	
+	public void printSortByPublisherMessage() {
+		
+	}
 }
