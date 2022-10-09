561810440bd53a103e1f51099b931c962a90a3c5
IvanOmelchenkoIP <io25112002@gmail.com>
2022-10-09 22:31:24 +0300

    Add methods to BookArrayController
    Add methods to BookArrayController for further implementation

--- java/src/lab3/BookArrayController.java
+++ java/src/lab3/BookArrayController.java
@@ -1,5 +1,32 @@
 package lab3;
 
 public class BookArrayController {
-
+	BookArrayModel model;
+	BookArrayView view;
+	
+	BookArrayController(BookArrayModel model, BookArrayView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void initBookArray(Book[] books) {
+		for (Book book : books) model.addBook(book);
+		view.printInitMessage(books.length);
+	}
+	
+	public void getBooksOfAuthor() {
+		
+	}
+	
+	public void getBooksOfPublisher() {
+		
+	}
+	
+	public void getBooksAfterYear() {
+		
+	}
+	
+	public void sortBooksByPublisher() {
+		
+	}
 }
